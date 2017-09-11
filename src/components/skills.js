e86f9b14d672a64c58028b13a924fcd362c657ed
mattvox <mattbliffert@icloud.com>
2017-09-11 17:33:57 -0400

    ...

--- src/components/skills.js
+++ src/components/skills.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Col } from 'react-bootstrap'
 
-import Meter from 'grommet/components/Meter'
+// import Meter from 'grommet/components/Meter'
 
 import ProgressBar from 'react-progressbar.js'
 
